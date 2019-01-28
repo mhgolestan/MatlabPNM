b898e7fc58e7b23242781b6d0e8ff339e60a8abc
Mohammad Hossein Golestan <golestan1369@gmail.com>
2019-01-28 21:08:56 +0100

    input folder added

--- main.m
+++ main.m
@@ -1,27 +1,36 @@
-clc; clear
-fileName = 'Berea';
+% Clearing the workspace
+clear
+% clearing the current window
+clc
+% Getting the working directory
+currentFoldet = pwd;
+
+networkFileName = 'C1';
+networkFileFullPath = strcat(currentFoldet, '\Input\NetworkDataFile\' , networkFileName);
+
 
 % Pressure difference
 inletPressure = 1;
 outletPressure = 0;
 
-network = Network(fileName);
-% network.calculatePorosity();
-% fprintf('Porosity of the model is: %3.5f \n', network.Porosity);
- 
+network = Network(networkFileFullPath);
+network.calculatePorosity();
+fprintf('Porosity of the model is: %3.5f \n', network.Porosity);
+
 % network.pressureDistribution(1,0);
 % network.calculateAbsolutePermeability();
+% fprintf('%3.5f \n', network.absolutePermeability)
 % network.calculateRelativePermeability();
 network.PrimaryDrainage();
 
-% press = zeros(network.numberOfNodes,1);
-% x = zeros(network.numberOfNodes,1);
-% index = zeros(network.numberOfNodes,1);
-% for ii = 1:network.numberOfNodes
-%     if network.Nodes{ii}.y_coordinate == network.Nodes{1}.y_coordinate
-%     press(ii) = network.Nodes{ii}.waterPressure;
-%     index(ii) = network.Nodes{ii}.index;
-%     x(ii) = network.Nodes{ii}.x_coordinate;
-%     end
-% end
-% plot(x, press, '*')
+press = zeros(network.numberOfNodes,1);
+x = zeros(network.numberOfNodes,1);
+index = zeros(network.numberOfNodes,1);
+for ii = 1:network.numberOfNodes
+    if network.Nodes{ii}.y_coordinate == network.Nodes{1}.y_coordinate
+    press(ii) = network.Nodes{ii}.waterPressure;
+    index(ii) = network.Nodes{ii}.index;
+    x(ii) = network.Nodes{ii}.x_coordinate;
+    end
+end
+plot(x, press, '*')
