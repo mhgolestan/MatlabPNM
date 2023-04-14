be4713a07564158785b3ecc54c89b14e77509845
Leila Hashemi <leilahashemi@users.noreply.github.com>
2023-04-14 14:57:52 +0200

    modify test!

---
+++ src/test_PoreNetworkFlow.m
@@ -0,0 +1,59 @@
+classdef test_PoreNetworkFlow < matlab.unittest.TestCase
+    
+    properties
+        mynetwork
+    end
+    
+    methods (TestMethodSetup)
+        function createmynetwork(testCase)
+            
+            import quasiStatic.*
+            % Crearing an object of the mynetwork
+            mynetworkFileName = 'simple_9_homogen_highAR';  
+            testCase.mynetwork = Network(mynetworkFileName);
+        end
+    end
+    
+    methods (Test)
+        
+        function testSinglePhaseFlow(testCase)
+            % Test single-phase flow simulation
+            testCase.mynetwork.calculateSinglePhasePressureDistribution = true;
+            testCase.mynetwork.inletPressure_Pa = 1;
+            testCase.mynetwork.outletPressure_Pa = 0;
+            testCase.mynetwork.calculateRelativePermeability = false;
+            
+            % Verify that pressure distribution is calculated
+            testCase.verifyTrue(testCase.mynetwork.calculateSinglePhasePressureDistribution);
+            % Verify that inlet and outlet pressures are set correctly
+            testCase.verifyEqual(testCase.mynetwork.inletPressure_Pa, 1);
+            testCase.verifyEqual(testCase.mynetwork.outletPressure_Pa, 0);
+            % Verify that relative permeability is not calculated
+            testCase.verifyFalse(testCase.mynetwork.calculateRelativePermeability);
+        end
+        
+        function testTwoPhaseFlow(testCase)
+            % Test two-phase flow simulation
+            testCase.mynetwork.calculateSinglePhasePressureDistribution = false;
+            testCase.mynetwork.calculateRelativePermeability = true;
+            testCase.mynetwork.inletPressure_Pa = 1;
+            testCase.mynetwork.outletPressure_Pa = 0;
+            testCase.mynetwork.recedingContactAngle = 0;
+            testCase.mynetwork.advancingContactAngle = 0;
+            
+            testCase.mynetwork.primaryDrainage_20191207();
+            testCase.mynetwork.secondaryImbibition_20191207();
+            
+            % Verify that relative permeability is calculated
+            testCase.verifyTrue(testCase.mynetwork.calculateRelativePermeability);
+            % Verify that inlet and outlet pressures are set correctly
+            testCase.verifyEqual(testCase.mynetwork.inletPressure_Pa, 1);
+            testCase.verifyEqual(testCase.mynetwork.outletPressure_Pa, 0);
+            % Verify that contact angles are set correctly
+            testCase.verifyEqual(testCase.mynetwork.recedingContactAngle, 0);
+            testCase.verifyEqual(testCase.mynetwork.advancingContactAngle, 0);
+        end
+        
+    end
+    
+end
